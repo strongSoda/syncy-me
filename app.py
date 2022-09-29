1860794ace759792af6aa3ff90b6cf0b4949c339
strongSoda <strongsoda2@gmail.com>
2022-09-29 19:56:05 +0530

    gunicorn setup

--- app.py
+++ app.py
@@ -8,19 +8,11 @@ app = Flask(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-# @app.route('/')
-# def home():
-#     return jsonify({'message': 'Hello World!'})
-
-
 # render the index.html file
 @app.route('/')
 def index():
     # render the index.html file
     return send_file('index.html')
-
-
-
 
 
 if __name__ == '__main__':
